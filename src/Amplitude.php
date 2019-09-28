7d7b925025288dcd918081dcf5af5d4603a181a9
Francesco Malatesta <hellofrancesco@gmail.com>
2019-09-28 15:57:07 +0200

    Add main service class

---
+++ src/Amplitude.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace LaravelAmplitude;
+
+use LaravelAmplitude\Drivers\AmplitudeDriverInterface;
+
+class Amplitude
+{
+    /** @var AmplitudeDriverInterface */
+    private $driver;
+
+    /**
+     * Amplitude constructor.
+     *
+     * @param AmplitudeDriverInterface $driver
+     */
+    public function __construct(AmplitudeDriverInterface $driver)
+    {
+        $this->driver = $driver;
+    }
+
+    public function init($apiKey)
+    {
+        $this->driver->init($apiKey);
+    }
+
+    public function setUserId($userId)
+    {
+        $this->driver->setUserId($userId);
+    }
+
+    public function setUserProperties($userProperties)
+    {
+        $this->driver->setUserProperties($userProperties);
+    }
+
+    public function sendEvent($name, $properties = [])
+    {
+        $this->driver->sendEvent($name, $properties);
+    }
+}
---
+++ tests/AmplitudeTest.php
@@ -0,0 +1,88 @@
+<?php
+
+
+namespace LaravelAmplitude\Tests;
+
+
+use LaravelAmplitude\Amplitude;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+use LaravelAmplitude\Drivers\AmplitudeDriverInterface;
+
+class AmplitudeTest extends TestCase
+{
+    /** @var MockObject | AmplitudeDriverInterface */
+    private $driver;
+
+    /** @var Amplitude */
+    private $amplitude;
+
+    public function setUp()
+    {
+        $this->driver = $this->getMockBuilder(AmplitudeDriverInterface::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->amplitude = new Amplitude($this->driver);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_init()
+    {
+        $this->driver->expects(self::once())
+            ->method('init')
+            ->with('APIKEY');
+
+        $this->amplitude->init('APIKEY');
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_set_user_id()
+    {
+        $this->driver->expects(self::once())
+            ->method('setUserId')
+            ->with('ID');
+
+        $this->amplitude->setUserId('ID');
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_set_user_properties()
+    {
+        $this->driver->expects(self::once())
+            ->method('setUserProperties')
+            ->with(['property' => 'value']);
+
+        $this->amplitude->setUserProperties(['property' => 'value']);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_send_event()
+    {
+        $this->driver->expects(self::once())
+            ->method('sendEvent')
+            ->with('name', ['prop' => '123']);
+
+        $this->amplitude->sendEvent('name', ['prop' => '123']);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_send_event_without_properties()
+    {
+        $this->driver->expects(self::once())
+            ->method('sendEvent')
+            ->with('name', []);
+
+        $this->amplitude->sendEvent('name');
+    }
+}
