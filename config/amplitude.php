b3f011809c3625174cfce1b3f25f291d8349f199
Francesco Malatesta <hellofrancesco@gmail.com>
2019-09-28 16:37:22 +0200

    Add some docs to config

--- config/amplitude.php
+++ config/amplitude.php
@@ -1,6 +1,28 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Driver
+    |--------------------------------------------------------------------------
+    |
+    | The driver you want to use to send your data. You can choose from the
+    | following list:
+    |
+    | - amplitude
+    | - log
+    | - null
+    |
+    */
     'driver' => 'amplitude',
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Key
+    |--------------------------------------------------------------------------
+    |
+    | The API Key of your Amplitude project.
+    |
+    */
     'api_key' => env('AMPLITUDE_API_KEY')
 ];
